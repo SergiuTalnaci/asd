82c2f2c2ab6603b89bb224d9a6dc70681966bcff
Sergiu Vlad <sergiu230@gmail.com>
2017-04-09 23:12:03 +0200

    1st

---
+++ ClientApp/app/components/weather/weather.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'weather',
+    templateUrl: './weather.component.html'
+})
+export class WeatherComponent {
+    public weather: Weather;
+
+    constructor(http: Http) {
+        http.get('/api/weather/city/Hobro').subscribe(result => {
+            this.weather = result.json();
+            console.log(this.weather);
+        });
+    }
+}
+
+interface Weather {
+    temp: number;
+    summary: string;
+    city: string;
+}
